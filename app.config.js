51582e6bbada6eebaf2f9862c57d91efc39387d0
Stênio do Carmo <steniobotelhodocarmo@gmail.com>
2023-08-10 19:49:18 -0300

    build: configuring env files

--- app.config.js
+++ app.config.js
@@ -1,4 +1,8 @@
-{
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+module.exports = {
   "expo": {
     "name": "girottosmap",
     "slug": "girottosmap",
@@ -19,16 +23,32 @@
       "**/*"
     ],
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "config":{
+        "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY
+      }
     },
     "android": {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
+      },
+      "config": {
+        "googleMaps": {
+          "apiKey": process.env.GOOGLE_MAPS_API_KEY
+        }
       }
     },
     "web": {
       "favicon": "./assets/favicon.png"
-    }
+    },
+    "plugins": [
+      [
+        "expo-location",
+        {
+          "locationAlwaysAndWhenInUsePermission": "Allow $(PRODUCT_NAME) to use your location."
+        }
+      ]
+    ]
   }
 }
