6fb6a0f67614ce003c8eec58317f7a2b0f398b12
Stênio do Carmo <steniobotelhodocarmo@gmail.com>
2023-09-04 18:50:32 -0300

    feat: log out button

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,11 +1,12 @@
 import { Input, HStack, IconButton } from "native-base";
+import { Feather } from '@expo/vector-icons'; 
 
-export function SearchBar() {
+export function SearchBar({...rest}) {
   return(
-    <HStack mx={4} mt={16} alignItems="center">
+    <HStack mx={4} mt={8} alignItems="center">
       <Input 
       placeholder="Buscar local" 
-      w="100%"
+      w={72}
       h={14}
       borderRadius="4" 
       py="3" 
@@ -15,6 +16,21 @@ export function SearchBar() {
       placeholderTextColor="black.50"
       bgColor="white"
     />
+    <IconButton 
+          size={14}
+          _icon={{
+            as: Feather,
+            name: "log-out",
+            size: 7,
+            color: "white"
+          }}
+          bgColor="green.100"
+          borderRadius="100"
+          borderWidth="1"
+          borderColor="blue.100"
+          mx={6}
+          {...rest}
+        />
     </HStack>
   )
 }
